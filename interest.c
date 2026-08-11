856b9872e6542475858b70b673c62fc5d27bebea
Jaymin-Patel9 <jayminpatel99134@gmail.com>
2026-08-11 18:12:23 +0530

    Add interest calculation program in C

---
+++ interest.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+  float i,p,r,n;
+  printf("interest rate\n");
+  printf("Enter principle value: ");
+  scanf("%f",&p);
+  printf("Enter rate of interest: ");
+  scanf("%f",&r);
+  printf("Enter number of years: ");
+  scanf("%f",&n);
+  
+  i=(r*p*n)/100;
+
+  printf("interest rate is calculated to be %f",i);
+
+      return 0;
+}
