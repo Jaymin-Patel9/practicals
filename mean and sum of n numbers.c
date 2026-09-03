569ae0b28a1b28f2c98d85ebf20e147cded08ba4
Jaymin-Patel9 <jayminpatel99134@gmail.com>
2026-09-03 21:36:40 +0530

    Refactor sum calculation logic and formatting
    Refactor input handling for sum calculation and improve formatting.

--- mean and sum of n numbers.c
+++ mean and sum of n numbers.c
@@ -8,43 +8,35 @@ int main(){
 
         printf("Calculate sum and mean of any n values.:");
         scanf("%d",&n);
-
-         printf("\n");
+        printf("\n");
         printf("while loop\n");
        
 
         while(i<=n){
 
-
              printf("enter nums:");
              scanf("%d",&num);
-
                 sum=sum+num;
-               
-               
                 i++;
 
-        }
+             }
+       
         printf("sum is %d\n",sum);
-
         printf("\n");
         printf("for loop\n");
         sum=0;
 
 
         for(i=1;i<=n;i++){
+                
              printf("enter nums:");
              scanf("%d",&num);
-
-                sum=sum+num;
-
-               
-                
-        }
-        printf("sum is %d\n",sum);
-
-        mean=sum/(n*1.0);
+             sum=sum+num;
+                         
+            }
         
+        printf("sum is %d\n",sum);
+        mean=sum/(n*1.0);
         printf("mean is %f",mean);
          
 
