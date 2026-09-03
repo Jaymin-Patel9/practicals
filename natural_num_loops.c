ea256ebd66457743107f3b3d0c2cf550b020c188
Jaymin-Patel9 <jayminpatel99134@gmail.com>
2026-09-03 20:26:15 +0530

    Add program to print first 10 natural numbers
    Prints the first 10 natural numbers using both while and for loops.

---
+++ natural_num_loops.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+
+        int i=1;
+
+        printf("first 10 natural numbers\n");
+        printf("while loop\n");
+
+        while(i<=10){
+
+                printf("%d\n",i);
+                i++;
+
+        }
+
+     
+         printf("for loop\n");
+
+        for(i=1;i<=10;i++){
+
+                printf("%d\n",i);
+        }
+
+
+
+
+return 0;
+}
