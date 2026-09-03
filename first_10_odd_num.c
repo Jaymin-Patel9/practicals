c4de1d14816be312d7066bf7760c18964cfc197e
Jaymin-Patel9 <jayminpatel99134@gmail.com>
2026-09-03 20:28:47 +0530

    Add program to print first 10 odd numbers
    Prints the first 10 odd numbers using both while and for loops.

---
+++ first_10_odd_num.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+
+        int i=1;
+
+        printf("first 10 odd numbers\n");
+        printf("while loop\n");
+
+        while(i<=20){
+
+                printf("%d\n",i);
+                i=i+2;
+
+        }
+
+   
+         printf("for loop\n");
+
+        for(i=1;i<=20;i=i+2){
+
+                printf("%d\n",i);
+        }
+
+return 0;
+}
