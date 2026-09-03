81367851bbbb2ee083ca3cbada60017488722993
Jaymin-Patel9 <jayminpatel99134@gmail.com>
2026-09-03 21:34:22 +0530

    Add program to calculate sum and mean of n numbers
    This program calculates the sum and mean of n numbers using both a while loop and a for loop.

---
+++ mean and sum of n numbers.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main(){
+
+        int n,i=1,sum=0,num;
+        float mean;
+
+
+        printf("Calculate sum and mean of any n values.:");
+        scanf("%d",&n);
+
+         printf("\n");
+        printf("while loop\n");
+       
+
+        while(i<=n){
+
+
+             printf("enter nums:");
+             scanf("%d",&num);
+
+                sum=sum+num;
+               
+               
+                i++;
+
+        }
+        printf("sum is %d\n",sum);
+
+        printf("\n");
+        printf("for loop\n");
+        sum=0;
+
+
+        for(i=1;i<=n;i++){
+             printf("enter nums:");
+             scanf("%d",&num);
+
+                sum=sum+num;
+
+               
+                
+        }
+        printf("sum is %d\n",sum);
+
+        mean=sum/(n*1.0);
+        
+        printf("mean is %f",mean);
+         
+
+return 0;
+}
