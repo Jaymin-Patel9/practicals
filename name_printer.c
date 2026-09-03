13be7a1599fdb5b9a70bf469df932222812f004a
Jaymin-Patel9 <jayminpatel99134@gmail.com>
2026-09-03 20:54:51 +0530

    Add name printing using while and for loops

---
+++ name_printer.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+
+        int n,i=1;
+
+
+        printf("print name how many times?:");
+        scanf("%d",&n);
+
+         printf("\n");
+        printf("while loop\n");
+       
+
+        while(i<=n){
+
+                printf("jaymin patel\n");
+                
+                i++;
+
+        }
+
+        printf("\n");
+        printf("for loop\n");
+
+
+        for(i=1;i<=n;i++){
+
+                 printf("jaymin patel\n");
+                
+        }
+         
+
+return 0;
+}
